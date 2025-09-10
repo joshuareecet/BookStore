1af60e7875e17adbaf9922008d1d0e53d25d13c7
Joshua <jrtbing@gmail.com>
2025-09-10 23:22:57 +0100

    Added book searching to main

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 import config
-from web.search import conductSearch
+from web import search , web_book_converter
+from user_library.storage import Book , BookLibrary
 
 def main():
-  conductSearch()
+ search_results = search.conductSearch()
+ tempLibrary = BookLibrary()
+ web_book_converter.convertToLibrary(search_results, tempLibrary)
+ i = 1
+ print("Books found include: \n")
+ for books in tempLibrary.books():
+  print(f"{i}: {books.title()}")
+  i += 1
 
-
+main()
